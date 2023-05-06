f10821d73f9976792f1a0ca41e213b833902e4a3
mikuweb <mxxx.kwtmg@gmail.com>
2023-05-06 13:32:21 +0900

    make Navbar responsive

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-
+import { BiMenu } from "react-icons/bi";
+// 15:05
 const Navbar = () => {
   return (
     <nav className="bg-blue-900 h-11 flex justify-between items-center px-6">
@@ -9,8 +10,8 @@ const Navbar = () => {
         <span className="text-white font-extrabold">App</span>
       </div>
       {/* LINK ITEMS */}
-      <div>
-        <ul className="flex justify-end gap-4">
+      <div className="md:static absolute bg-blue-900 bg-opacity-90 md:min-h-fit min-h-[60vh] left-0 top-[44px] w-full md:w-auto flex items-center px-5">
+        <ul className="flex flex-col md:flex-row md:items-center justify-end gap-8 md:gap-4">
           <li>
             <a
               className="text-white rounded-full py-1 px-2 hover:border-2 active:border-2 active:border-cyan-400"
@@ -28,6 +29,10 @@ const Navbar = () => {
             </a>
           </li>
         </ul>
+      </div>
+      {/* MENU ICON */}
+      <div className="md:hidden">
+        <BiMenu color="white" size={28} />
       </div>
     </nav>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
-import Navbar from "../../components/Navbar";
+import Navbar from "../components/Navbar";
+
 
 export default function Home() {
   return (
     <>
       <Navbar />
-      <main className="flex min-h-screen flex-col items-center p-24">
+      
+      <main className="p-24 bg-gradient-to-br from-sky-100 via-cyan-100 to-cyan-200 flex min-h-screen flex-col items-center ">
         <div className="bg-blue-100">Hello World!</div>
       </main>
     </>
