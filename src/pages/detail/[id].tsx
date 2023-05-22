2aa756eea113131cc21045e195d993569e5ebc16
mikuweb <mxxx.kwtmg@gmail.com>
2023-05-22 14:06:05 +0900

    get dynamic detail data for [id].tsx

--- src/pages/detail/[id].tsx
+++ src/pages/detail/[id].tsx
@@ -2,8 +2,10 @@ import React from "react";
 import Button from "@/components/Button";
 import { useRouter } from "next/router";
 import { IoIosArrowDropleftCircle } from "react-icons/io";
+import Image from "next/image";
 
-const Detail = () => {
+const Detail = ({ data }) => {
+  console.log(data);
   const router = useRouter();
   const id = router.query.id;
 
@@ -19,26 +21,35 @@ const Detail = () => {
       <div className="bg-green-100/50 flex flex-col items-center h-screen">
         {/* White Container */}
         <div className="mb-10 md:mt-14 bg-white overflow-hidden w-full md:w-9/12 md:max-w-screen-lg text-center items-center">
-          <div className="text-4xl font-semibold mt-4">Pikachu</div>
-          <div className="text-2xl text-slate-700 mb-4">#0025</div>
+          <div className="text-4xl font-semibold mt-4">{data.name}</div>
+          <div className="text-2xl text-slate-700 mb-4">{`# ${(
+            "000" + id
+          ).slice(-4)}`}</div>
           {/* Container */}
           <div className="flex flex-col md:flex-row">
             {/*Left Img */}
             <div className="md:w-1/2 flex items-center">
-              <div className="w-60 h-60 bg-slate-200 mx-auto">Img</div>
+              <div className="w-60 h-60 bg-slate-200 mx-auto flex justify-center items-start">
+                <Image
+                  src={data.sprites.other.home.front_default}
+                  alt={data.name}
+                  width={200}
+                  height={200}
+                />
+              </div>
             </div>
             {/*Right Contents */}
             <div className="w-full py-10 md:w-1/2 flex flex-col items-center gap-4 mx-auto ">
-              <div className="bg-sky-300/70 w-3/4 rounded-lg py-5">
+              <div className="bg-sky-300 w-3/4 rounded-lg py-5">
                 <table className="table-auto w-2/3 mx-auto border-separate border-spacing-2">
                   <tbody className="text-start">
                     <tr>
                       <td className="text-xl font-semibold">Height</td>
-                      <td className="text-xl">40 cm</td>
+                      <td className="text-xl">{`${data.height} cm`}</td>
                     </tr>
                     <tr>
                       <td className="text-xl font-semibold">Weight</td>
-                      <td className="text-xl">6 kg</td>
+                      <td className="text-xl">{`${data.weight} kg`}</td>
                     </tr>
                   </tbody>
                 </table>
@@ -46,9 +57,10 @@ const Detail = () => {
 
               <div className="flex flex-col justify-between">
                 <span className="font-semibold text-xl mb-2">Type</span>
-                <span className="bg-yellow-400/70 py-2 px-5 rounded-lg">
-                  Electric
-                </span>
+                {data.types.map(type => (<span key={type.type.name} className="bg-yellow-400 py-2 px-5 rounded-lg">
+                  {type.type.name}
+                </span>))}
+                
               </div>
             </div>
             {/* TEST */}
@@ -73,7 +85,6 @@ export default Detail;
 
 // getServerSideProps getStaticPropsとほぼ同じ書き方
 export async function getServerSideProps(context) {
-  
   const { id } = context.query; // = const id = context.query.id;
   const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
   const data = await response.json();
