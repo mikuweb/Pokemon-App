ea33ad7a8c532950fa7e53a9088616ef3d14470f
mikuweb <mxxx.kwtmg@gmail.com>
2023-05-22 14:22:53 +0900

    fix unit for Pokemon height & weight in [id].tsx

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -16,7 +16,7 @@ const Button: React.FC<ButtonProps> = ({
   return (
     <button
       onClick={onClick}
-      className={`${className} px-3 py-1 text-base md:text-lg flex items-center justify-center gap-1 font-semibold border-2 border-blue-600 bg-white hover:bg-blue-100 active:border-cyan-400 text-blue-600 rounded-full`}
+      className={`${className} px-3 py-1 text-base md:text-lg flex items-center justify-center gap-1 font-semibold border-2 border-blue-600 bg-white hover:opacity-70 active:border-cyan-400 text-blue-600 rounded-full`}
       disabled={disabled}
     >
       {children}
--- src/pages/detail/[id].tsx
+++ src/pages/detail/[id].tsx
@@ -4,8 +4,7 @@ import { useRouter } from "next/router";
 import { IoIosArrowDropleftCircle } from "react-icons/io";
 import Image from "next/image";
 
-const Detail = ({ data }) => {
-  console.log(data);
+const Detail = ({ name, img, height, weight, types }) => {
   const router = useRouter();
   const id = router.query.id;
 
@@ -21,21 +20,18 @@ const Detail = ({ data }) => {
       <div className="bg-green-100/50 flex flex-col items-center h-screen">
         {/* White Container */}
         <div className="mb-10 md:mt-14 bg-white overflow-hidden w-full md:w-9/12 md:max-w-screen-lg text-center items-center">
-          <div className="text-4xl font-semibold mt-4">{data.name}</div>
-          <div className="text-2xl text-slate-700 mb-4">{`# ${(
-            "000" + id
-          ).slice(-4)}`}</div>
+          <div className="text-4xl font-semibold mt-4 uppercase mb-2">
+            {name}
+          </div>
+          <div className="text-2xl text-slate-700">{`# ${("000" + id).slice(
+            -4
+          )}`}</div>
           {/* Container */}
           <div className="flex flex-col md:flex-row">
             {/*Left Img */}
             <div className="md:w-1/2 flex items-center">
-              <div className="w-60 h-60 bg-slate-200 mx-auto flex justify-center items-start">
-                <Image
-                  src={data.sprites.other.home.front_default}
-                  alt={data.name}
-                  width={200}
-                  height={200}
-                />
+              <div className="w-60 h-60 bg-slate-200 mx-auto flex justify-center items-start rounded-lg">
+                <Image src={img} alt={name} width={200} height={200} />
               </div>
             </div>
             {/*Right Contents */}
@@ -45,22 +41,28 @@ const Detail = ({ data }) => {
                   <tbody className="text-start">
                     <tr>
                       <td className="text-xl font-semibold">Height</td>
-                      <td className="text-xl">{`${data.height} cm`}</td>
+                      <td className="text-xl">{`${height} cm`}</td>
                     </tr>
                     <tr>
                       <td className="text-xl font-semibold">Weight</td>
-                      <td className="text-xl">{`${data.weight} kg`}</td>
+                      <td className="text-xl">{`${weight} kg`}</td>
                     </tr>
                   </tbody>
                 </table>
               </div>
 
-              <div className="flex flex-col justify-between">
+              <div className="w-3/4 flex flex-col justify-between">
                 <span className="font-semibold text-xl mb-2">Type</span>
-                {data.types.map(type => (<span key={type.type.name} className="bg-yellow-400 py-2 px-5 rounded-lg">
-                  {type.type.name}
-                </span>))}
-                
+                <div className="flex justify-center gap-10">
+                  {types.map((type) => (
+                    <span
+                      key={type.type.name}
+                      className="bg-yellow-400 py-2 px-5 rounded-lg text-xl"
+                    >
+                      {type.type.name}
+                    </span>
+                  ))}
+                </div>
               </div>
             </div>
             {/* TEST */}
@@ -88,10 +90,16 @@ export async function getServerSideProps(context) {
   const { id } = context.query; // = const id = context.query.id;
   const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
   const data = await response.json();
-  console.log(data);
+
+  const heightCm = data.height * 10;
+  const weightKg = data.weight / 10;
   return {
     props: {
-      data,
+      name: data.name,
+      img: data.sprites.other.home.front_default,
+      height: heightCm,
+      weight: weightKg,
+      types: data.types,
     },
   };
 }
